26397df0728893624ffabfbb09829c451e7f3324
Chakilam, Santhosh <devopsguys@gmail.com>
2021-07-02 08:44:48 +0530

    Added more text to hello world programme

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for Cloudnative IT Technologies";
+        return "DevOps Project for Cloudnative IT Technologies pvt ltd";
     }
 }
